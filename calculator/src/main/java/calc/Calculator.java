0bb37380d8c7c602df39431f2b42c2ed07d68257
olegapa <55486781+olegapa@users.noreply.github.com>
2021-07-15 01:12:42 +0300

    logs added

--- calculator/src/main/java/calc/Calculator.java
+++ calculator/src/main/java/calc/Calculator.java
@@ -1,8 +1,15 @@
 package calc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Calculator {
+    private static final Logger logger = LoggerFactory.getLogger(Calculator.class);
 
     public static long calculate(int elem1, int elem2, String operation){
+
+        logger.debug("Input: elem1 = {}\telem2 = {}\toperation - {}", elem1, elem2, operation);
+
         if(operation.equals("plus"))
             return elem1 + elem2;
 
